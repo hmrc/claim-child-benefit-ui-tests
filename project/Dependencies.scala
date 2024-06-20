b060ef6ba3e6407d5458654ae3a83cbcfe56939a
Vincent Vinnicombe <5462800+vinnicombe@users.noreply.github.com>
2024-06-20 11:35:13 +0100

    DDCNL-9128: Bump to latest

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     "com.vladsch.flexmark" % "flexmark-all"   % "0.62.2" % Test,
     "org.scalatest"       %% "scalatest"      % "3.2.17" % Test,
     "org.slf4j"            % "slf4j-simple"   % "1.7.36" % Test,
-    "uk.gov.hmrc"         %% "ui-test-runner" % "0.29.0" % Test,
+    "uk.gov.hmrc"         %% "ui-test-runner" % "0.31.0" % Test,
     "org.scalacheck"      %% "scalacheck"     % "1.17.0" % Test,
 
   )
