c90c5e0bab928643a38eff09e103cef19fc9b65b
annemonica5 <129970066+annemonica5@users.noreply.github.com>
2025-04-03 16:15:53 +0100

    Updated Dependencies for claim child benefits tests

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val test: Seq[ModuleID] = Seq(
     "com.vladsch.flexmark" % "flexmark-all"   % "0.64.8" % Test,
     "org.scalatest"       %% "scalatest"      % "3.2.19" % Test,
-    "org.slf4j"            % "slf4j-simple"   % "2.0.16" % Test,
+    "org.slf4j"            % "slf4j-simple"   % "2.0.17" % Test,
     "uk.gov.hmrc"         %% "ui-test-runner" % "0.45.0" % Test,
     "org.scalacheck"      %% "scalacheck"     % "1.18.1" % Test,
   )
